7706c65ee6eeab0cdfa4a07588d0d3acb572c481
Brian Chu <atuinblue@gmail.com>
2013-11-29 11:21:16 -0800

    added sizeX and sizeY to MObjs, began getting player tanks working.
    Also added particle bouncing against terrain.

--- scripts/BubbleGameDriver.js
+++ scripts/BubbleGameDriver.js
@@ -8,7 +8,17 @@ var ctx = display.getContext("2d");
 
 
 //COLORS
-var ROYAL_BLUE = "hsla(243, 69%, 38%, 1)";
+const ROYAL_BLUE = "hsla(243, 69%, 38%, 1)";
+const RED = "hsla(0, 72%, 50%, 1)";
+const DARK_BROWN = "hsla(23, 60%, 22%, 1)";
+const BROWN = "hsla(23, 60%, 47%, 1)";
+
+
+var p1 = new Tank(display.width/4, display.height/2, 0,0, RED);
+var p2 = new Tank(display.width * (3/4), display.height/2, 0,0, ROYAL_BLUE);
+var playerTankSize = 32;
+var tankGravity = 0.2;
+
 
 var projectiles = [];
 var missileGravity = 0.16;
@@ -16,29 +26,31 @@ var missileGravity = 0.16;
 var terrain = [];
 var terrainFallSpeed = .1;
 var terrainMask = [];
+var terrainChunkWidth = 4;
 
 var particles = [];
 var particleSize = 5;
 var particleGravity = .16;
-var particleFade = 0.01385;
+var particleFade = 0.01; //(1/72); //72 frames to disapear
 var minParticleSpeed = 1;
 var maxParticleSpeed = 12;
 var particlesPerBurst = 32;
 
 var particleTestTimer = 2000;
 var canSpawnParticlesTest = true;
-var particleTestSource = new MObj(display.width/2, display.height/2, 0, 0, 32, ROYAL_BLUE);
+var particleTestSource = new MObj(display.width/2, display.height/2, 0, 0, playerTankSize, playerTankSize, ROYAL_BLUE);
 
 
 //===========================================================================================
 
 
-function MObj(x,y,deltaX,deltaY,size,color){
+function MObj(x,y,deltaX,deltaY,sizeX,sizeY,color){
 	this.xPos = x;
 	this.yPos = y;
 	this.deltaX = deltaX;
 	this.deltaY = deltaY;
-	this.size = size;
+	this.sizeX = sizeX;
+	this.sizeY = sizeY;
 	this.color = color;
 
 	this.updatePhysics = function() {
@@ -48,27 +60,41 @@ function MObj(x,y,deltaX,deltaY,size,color){
 	
 	this.draw = function() {
 		ctx.fillStyle = this.color;
-		ctx.fillRect(this.xPos, this.yPos, this.size, this.size);
+		ctx.fillRect(this.xPos, this.yPos, this.sizeX, this.sizeY);
 	}
 	
 	this.getRight = function() {
-		return this.xPos + this.size;
+		return this.xPos + this.sizeX;
 	}
 	
 	this.getBottom = function() {
-		return this.yPos + this.size;
+		return this.yPos + this.sizeY;
 	}
 	
 	this.isIntersecting = function(otherThing) {
-		return !( (otherThing.xPos > this.getRight()) || (otherThing.getRight() < this.xPos) || (otherThing.yPos > this.getBottom()) || (otherThing.getBottom() < this.yPos) ); 
+		return !( (otherThing.xPos >= this.getRight()) || (otherThing.getRight() <= this.xPos) || (otherThing.yPos >= this.getBottom()) || (otherThing.getBottom() <= this.yPos) ); 
 	}
 
+	this.intersectsTerrain = function(){
+		for(var ii = 0; ii < terrain.length; ++ii){
+			if(this.isIntersecting(terrain[ii])){
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 
-function createExplosion(explodee, particleDensity) {
-	var centerX = explodee.xPos + explodee.size / 2;
-	var centerY = explodee.yPos + explodee.size / 2;
+function Tank(x,y,deltaX,deltaY,color){
+	this.solid = new MObj(x, y, deltaX, deltaY, playerTankSize, playerTankSize, color);
+	this.currentGun = "single";
+}
+
+
+function createExplosion(explodee, particleDensity, bounce) {
+	var centerX = explodee.xPos + explodee.sizeX / 2;
+	var centerY = explodee.yPos + explodee.sizeY / 2;
 
 	if(particleDensity == null){
 		var particleDensity = particlesPerBurst;
@@ -83,45 +109,21 @@ function createExplosion(explodee, particleDensity) {
 		var velocityX = Math.cos(randomAngle) * speed;
 		var velocityY = Math.sin(randomAngle) * speed;
 				
-		var particle = new MObj(centerX, centerY, velocityX, velocityY, particleSize, explodee.color);
+		var particle = new MObj(centerX, centerY, velocityX, velocityY, particleSize, particleSize, explodee.color);
 		particle.alpha = 1;
+		if(bounce != null){
+			particle.bounce = true;
+		}
 		particles.push(particle);
 	}
 }
 
-function updateParticles() {
-	for (var i = particles.length - 1; i >= 0; --i) {
-		particles[i].updatePhysics();
-		
-		// add some acceleration from gravity each frame.
-		particles[i].deltaY += particleGravity;
-		particles[i].alpha -= particleFade;
 
-		particles[i].color = incrementAlpha(particles[i].color, particles[i].alpha);
-
-		// remove particles that are no longer on screen.
-		var shouldRemoveParticle = false;
-	
-		if (particles[i].xPos <= -particles[i].size) {
-			shouldRemoveParticle = true;
-		} else if (particles[i].xPos > display.width) {
-			shouldRemoveParticle = true;
-		} else if (particles[i].yPos <= -particles[i].size) {
-			shouldRemoveParticle = true;
-		} else if (particles[i].yPos > display.height) {
-			shouldRemoveParticle = true;
-		}
-		
-		if (shouldRemoveParticle) {
-			particles.splice(i, 1);
-		}	
-	}
-}
 
 function incrementAlpha(hsLine, alpha){
 	//take apart the HSLA string representing the color and change the alpha value to the given alpha. return the reconstructed string
 	var newHsla = "";
-	var values = hsLine.split("("); //clean off the elements of the string that are not he desired HSLA values
+	var values = hsLine.split("("); //clean off the elements of the string that are not the desired HSLA values
 	values = values[1].split(")");
 	var bits = values[0].split(",");
 
@@ -131,21 +133,17 @@ function incrementAlpha(hsLine, alpha){
 	return newHsla;
 }
 
-
-
-
-
-
-//=======================================================================================
-//GAME SETUP / START
+//========================================================================================================================
+//WORLD INITIALIZATION
 
 (function buildTerrain(){
-	for(var ii = 0; ii < display.height/3; ii += 4){
-		for(var kk = 0; kk < display.width ; kk += 4 ){
-			var grain = new MObj(display.width - kk - 4, display.height - ii - 4, 0,0, 4, "hsla(19, 63%, 28%, 1)");
-			terrain.push(grain);
-		}
+
+
+	for(var kk = 0; kk < display.width ; kk += terrainChunkWidth ){
+		var grain = new MObj(display.width - kk - terrainChunkWidth, display.height - display.height/3, 0,0, terrainChunkWidth, display.height/3, BROWN);
+		terrain.push(grain);
 	}
+	
 
 	//create a render mask for terrain.
 	//keep track of hit locations and size of explosions, then remove that from the shape.
@@ -157,11 +155,20 @@ function incrementAlpha(hsLine, alpha){
 
 })();
 
+
+
+//=======================================================================================
+//GAME SETUP / START
+
+
 (function update(){
 	window.requestAnimationFrame(update, display);
 
+	updatePlayer(p1);
+	updatePlayer(p2);
+
 	if (canSpawnParticlesTest){
-		createExplosion(particleTestSource);
+		createExplosion(particleTestSource, particlesPerBurst, true);
 		canSpawnParticlesTest = false;
 		window.setTimeout( function() { canSpawnParticlesTest = true; }, particleTestTimer);
 	}
@@ -173,6 +180,64 @@ function incrementAlpha(hsLine, alpha){
 
 
 
+function updatePlayer(player){
+	player.solid.updatePhysics();
+	
+	if(player.solid.intersectsTerrain()){
+		player.solid.deltaY = 0;
+		player.solid.deltaX = 0;
+	} else{
+		player.solid.deltaY += tankGravity;
+	}
+
+
+}
+
+
+
+
+
+
+
+function updateParticles() {
+	for (var i = particles.length - 1; i >= 0; --i) {
+		particles[i].updatePhysics();
+		
+		// add some acceleration from gravity each frame.
+		particles[i].deltaY += particleGravity;
+		particles[i].alpha -= particleFade;
+
+		particles[i].color = incrementAlpha(particles[i].color, particles[i].alpha);
+
+		if(particles[i].bounce != null){
+			if(particles[i].intersectsTerrain()){
+				particles[i].deltaY = -particles[i].deltaY;
+			}
+		}
+
+		// remove particles that are no longer on screen.
+		var shouldRemoveParticle = false;
+	
+		if (particles[i].xPos <= -particles[i].sizeX) {
+			shouldRemoveParticle = true;
+		} else if (particles[i].xPos > display.width) {
+			shouldRemoveParticle = true;
+		} else if (particles[i].yPos <= -particles[i].sizeY) {
+			shouldRemoveParticle = true;
+		} else if (particles[i].yPos > display.height) {
+			shouldRemoveParticle = true;
+		}
+
+		if(particles[i].alpha <= 0){
+			shouldRemoveParticle = true;
+		}
+		
+		if (shouldRemoveParticle) {
+			particles.splice(i, 1);
+		}	
+	}
+}
+
 //=======================================================================================
 //RENDERING
 
@@ -180,6 +245,8 @@ function drawFrame(){
 	clearDisplay();
 	drawBackdrop();
 
+	p1.solid.draw();
+	p2.solid.draw();
 
 	drawParticles();
 	drawTerrain();
@@ -196,6 +263,11 @@ function drawBackdrop(){
 
 }
 
+
+
+
+
+
 function drawParticles() {
 	for (var i = 0; i < particles.length; ++i) {
 		particles[i].draw();
@@ -210,8 +282,8 @@ function drawTerrain() {
 	};
 
 	var grad = ctx.createLinearGradient(0,display.height,0,display.height/2);
-	grad.addColorStop(0,"hsla(23, 60%, 22%, 1)");
-	grad.addColorStop(1,"hsla(23, 60%, 47%, 1)");
+	grad.addColorStop(0,DARK_BROWN);
+	grad.addColorStop(1,BROWN);
 	ctx.fillStyle = grad;
 
 	ctx.fill();
